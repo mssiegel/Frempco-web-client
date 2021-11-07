1f87f942f709d735ef5200c9622d6c2341d53f85
shah62 <52537686+shah62@users.noreply.github.com>
2021-11-07 10:02:04 -0500

    create theme for material-ui

---
+++ src/theme/index.ts
@@ -0,0 +1,2 @@
+export { default as theme } from './muiTheme';
+export * from './muiTheme';
---
+++ src/theme/muiTheme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material';
+
+/**
+ * This is the default MUI theme used in the app
+ * Modify this to override the default theme options for MUI components
+ */
+const theme = createTheme();
+
+export default theme;
