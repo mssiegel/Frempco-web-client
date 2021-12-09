42e5922bca11869d41a4e85d28e8214a728ae80b
Moshe Siegel <siegel.moshes@gmail.com>
2021-12-09 00:14:55 -0500

    Activate classroom buttons
    - Added buttons to teacher admin page to activate or deactivate a classroom
    - Installed npm package `@mui/icons-material`
    - Refactored by renaming SOCKET_URL variable to SERVER_URL

--- pages/teacher/classroom/[classroomName].tsx
+++ pages/teacher/classroom/[classroomName].tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head';
 import { Typography } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { Button } from '@mui/material';
+import {
+  PowerOff as PowerOffIcon,
+  Power as PowerIcon,
+} from '@mui/icons-material';
 
 import { getAllClassroomNames } from '@utils/classrooms';
 import Layout from '@components/Layout';
@@ -24,7 +29,25 @@ export async function getStaticProps({ params }) {
 }
 
 export default function TeacherDashboard({ classroomName }) {
+  const apiUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1`;
   const socket = useContext(SocketContext);
+  const [isActiveClassroom, setIsActiveClassroom] = useState(false);
+
+  async function activateClassroom() {
+    const postResponse = await fetch(`${apiUrl}/classrooms/${classroomName}`, {
+      method: 'POST',
+    });
+    const { isActive } = await postResponse.json();
+    setIsActiveClassroom(isActive);
+  }
+
+  async function deactivateClassroom() {
+    const postResponse = await fetch(`${apiUrl}/classrooms/${classroomName}`, {
+      method: 'DELETE',
+    });
+    const { isActive } = await postResponse.json();
+    setIsActiveClassroom(isActive);
+  }
 
   return (
     <Layout>
@@ -34,13 +57,32 @@ export default function TeacherDashboard({ classroomName }) {
       </Head>
 
       <main>
-        <Typography
-          variant='h3'
-          sx={{ color: (theme) => theme.palette.common.white }}
-        >
-          Hello teacher! Welcome to your dashboard for your classroom named{' '}
-          {classroomName}! Your socket ID is {socket?.id ?? 'NO SOCKET FOUND'}
+        <Typography variant='h4' sx={{ color: 'white' }}>
+          Hello teacher! Welcome to your classroom: {classroomName}
         </Typography>
+        <Typography variant='h4' sx={{ color: 'white' }}>
+          Your socket ID is {socket?.id ?? 'NO SOCKET FOUND'}
+        </Typography>
+        {isActiveClassroom ? (
+          <Button
+            variant='contained'
+            size='large'
+            color='warning'
+            startIcon={<PowerOffIcon />}
+            onClick={deactivateClassroom}
+          >
+            Deactivate classroom
+          </Button>
+        ) : (
+          <Button
+            variant='contained'
+            size='large'
+            startIcon={<PowerIcon />}
+            onClick={activateClassroom}
+          >
+            Activate classroom
+          </Button>
+        )}
       </main>
     </Layout>
   );
