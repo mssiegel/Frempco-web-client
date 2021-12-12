1b0e3b611db389856cd24e65a9c76b523fe9c563
Moshe Siegel <siegel.moshes@gmail.com>
2021-12-12 11:52:58 -0500

    Added Navbar with Home link
    - Added a simple navbar with a link to return to the homepage

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Link from '@utilComponents/Link';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,6 +7,19 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <Box sx={{ background: '#87002a', minHeight: '100vh' }}>{children}</Box>
+    <Box sx={{ background: '#87002a', minHeight: '100vh', pl: 2 }}>
+      <Navbar />
+      {children}
+    </Box>
+  );
+}
+
+function Navbar() {
+  return (
+    <Typography variant='h4' sx={{ ml: 10, pt: 1, pb: 2 }}>
+      <Link href='/' underline='hover' color='white'>
+        Home
+      </Link>
+    </Typography>
   );
 }
