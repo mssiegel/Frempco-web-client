1de712cc27d4e98f8e9f1a4818b62b51170f53c3
Moshe Siegel <siegel.moshes@gmail.com>
2021-12-12 22:01:45 -0500

    Student can enter classroom
    Adds the process for students to enter their classroom. It uses `window.prompt()` for the sake of faster engineering time. The `window.prompt()` and `window.alert()` will need to be replaced by better visuals later.
    
    - A teacher can enter the teachers page via the homepage by typing in their classroom name and password
    - A student can enter the students page via the homepage by typing in their classroom name. If their classroom is activated, they will visit their page, otherwise they'll receive an alert message.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,56 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import {
+  School as SchoolIcon,
+  Lightbulb as LightbulbIcon,
+} from '@mui/icons-material';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import Link from '@utilComponents/Link';
 import Layout from '@components/Layout';
 import classrooms from 'data/classrooms.json';
 
 export default function Home() {
+  const router = useRouter();
+  const apiUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1`;
+
   const sampleClassroomName = classrooms[0].classroomName;
+
+  async function visitStudentsPage() {
+    const classroom = window.prompt('What classroom are you visiting?');
+    const classroomObj = getClassroom(classroom);
+    if (!classroomObj) return window.alert(`Invalid classroom: ${classroom}`);
+
+    const getResponse = await fetch(`${apiUrl}/classrooms/${classroom}`);
+    const { isActive } = await getResponse.json();
+    if (!isActive)
+      return window.alert(
+        `Classroom not activated: ${classroom}\n Please wait for your teacher to activate your classroom and try again.`,
+      );
+
+    const studentName = window.prompt('Classroom found! What is your name?');
+    // TODO: send socket request to notify the teacher
+
+    // Visit students page
+    router.push(`/student/classroom/${classroom}`);
+  }
+
+  function visitTeachersPage() {
+    const classroom = window.prompt('What classroom are you visiting?');
+    const classroomObj = getClassroom(classroom);
+    if (!classroomObj) return window.alert(`Invalid classroom: ${classroom}`);
+
+    const password = window.prompt(`What is the password for ${classroom}?`);
+    if (String(classroomObj.password) !== password)
+      return window.alert(`IncorrectPassword: ${password}`);
+
+    // Visit teachers page
+    router.push(`/teacher/classroom/${classroom}`);
+  }
+
+  function getClassroom(classroom) {
+    return classrooms.find(({ classroomName }) => classroomName === classroom);
+  }
 
   return (
     <Layout>
@@ -26,16 +70,46 @@ export default function Home() {
         >
           Welcome to Frempco!
         </Typography>
-        <Typography variant='h3' sx={{ m: 5 }}>
-          <Link href={`/teacher/classroom/${sampleClassroomName}`}>
-            Visit Teachers admin page
-          </Link>
-        </Typography>
-        <Typography variant='h3' sx={{ m: 5 }}>
-          <Link href={`/student/classroom/${sampleClassroomName}`}>
-            Visit Students classroom page
-          </Link>
-        </Typography>
+        <Box sx={{ m: 5 }}>
+          <Typography variant='h3' sx={{ color: 'white', mb: 1 }}>
+            For Students:
+          </Typography>
+          <Button
+            variant='contained'
+            size='large'
+            startIcon={<LightbulbIcon />}
+            onClick={visitStudentsPage}
+          >
+            Students page
+          </Button>
+
+          <Typography variant='h3' sx={{ color: 'white', mb: 1, mt: 8 }}>
+            For Teachers:
+          </Typography>
+          <Button
+            variant='contained'
+            size='large'
+            startIcon={<SchoolIcon />}
+            onClick={visitTeachersPage}
+          >
+            Teachers page
+          </Button>
+
+          <Typography variant='h5' sx={{ m: 5, mt: 10, color: 'gray' }}>
+            Below link shortcuts are for development purposes only and will be
+            deleted prior to making this website live and ready for teachers
+          </Typography>
+          <Typography variant='h5' sx={{ m: 5 }}>
+            <Link href={`/teacher/classroom/${sampleClassroomName}`}>
+              Visit Teachers admin page
+            </Link>
+          </Typography>
+          <Typography variant='h5' sx={{ m: 5 }}>
+            <Link href={`/student/classroom/${sampleClassroomName}`}>
+              Visit Students classroom page
+            </Link>
+          </Typography>
+        </Box>
       </main>
     </Layout>
   );
