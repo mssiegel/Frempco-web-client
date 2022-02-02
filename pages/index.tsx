761c799b973f018f8ce0423c61a12555e27884f9
Moshe Siegel <siegel.moshes@gmail.com>
2022-02-01 21:35:59 -0500

    Added example chatbox to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import {
   School as SchoolIcon,
   Lightbulb as LightbulbIcon,
@@ -11,6 +11,38 @@ import Link from '@components/shared/Link';
 import Layout from '@components/shared/Layout';
 import { getClassroom, sampleClassroomName } from '@utils/classrooms';
 import { SocketContext } from '@contexts/SocketContext';
+import Chatbox from '@components/pages/TeachersPage/Chatbox';
+
+const exampleChat = {
+  chatId: 'as343da11sf#as31afdsf',
+  studentPair: [
+    {
+      socketId: '343d11sf',
+      realName: 'Sam Carlome',
+      character: 'Perfectionist Dentist',
+    },
+    {
+      socketId: 'as31afsf',
+      realName: 'Jessica Placard',
+      character: 'Party Planner',
+    },
+  ],
+  conversation: [
+    [
+      'student1',
+      'Perfectionist Dentist',
+      "I'm finally graduating dental school!!!",
+    ],
+    ['student2', 'Party Planner', 'This calls for a party!!!!!'],
+    ['student1', 'Perfectionist Dentist', 'YESSS!!'],
+    ['student1', 'Perfectionist Dentist', 'I am so excited!'],
+    ['student2', 'Party Planner', 'I can bring party food'],
+    ['student2', 'Party Planner', 'pretzels and chocolate cake'],
+    ['student1', 'Perfectionist Dentist', 'NO!!'],
+    ['student1', 'Perfectionist Dentist', "those aren't healthy"],
+  ],
+  startTime: '',
+};
 
 export default function Home() {
   const router = useRouter();
@@ -68,49 +100,60 @@ export default function Home() {
         >
           Welcome to Frempco!
         </Typography>
-        <Box sx={{ m: 5 }}>
-          <Typography variant='h3' sx={{ color: 'white', mb: 1 }}>
-            For Students:
-          </Typography>
-          <Button
-            variant='contained'
-            size='large'
-            startIcon={<LightbulbIcon />}
-            onClick={visitStudentsPage}
-          >
-            Students page
-          </Button>
 
-          <Typography variant='h3' sx={{ color: 'white', mb: 1, mt: 8 }}>
-            For Teachers:
-          </Typography>
-          <Button
-            variant='contained'
-            size='large'
-            startIcon={<SchoolIcon />}
-            onClick={visitTeachersPage}
-          >
-            Teachers page
-          </Button>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Box sx={{ m: 5 }}>
+              <Typography variant='h3' sx={{ color: 'white', mb: 1 }}>
+                For Students:
+              </Typography>
+              <Button
+                variant='contained'
+                size='large'
+                startIcon={<LightbulbIcon />}
+                onClick={visitStudentsPage}
+              >
+                Students page
+              </Button>
 
-          {process.env.NEXT_PUBLIC_NODE_ENV === 'development' && (
-            <>
-              <Typography variant='h5' sx={{ m: 3, mt: 10, color: 'gray' }}>
-                These link shortcuts only appear in the development environment:
+              <Typography variant='h3' sx={{ color: 'white', mb: 1, mt: 8 }}>
+                For Teachers:
               </Typography>
-              <Typography variant='h5' sx={{ m: 3 }}>
-                <Link href={`/teacher/classroom/${sampleClassroomName}`}>
-                  Visit Teachers admin page
-                </Link>
-              </Typography>
-              <Typography variant='h5' sx={{ m: 3 }}>
-                <Link href={`/student/classroom/${sampleClassroomName}`}>
-                  Visit Students classroom page
-                </Link>
-              </Typography>
-            </>
-          )}
-        </Box>
+              <Button
+                variant='contained'
+                size='large'
+                startIcon={<SchoolIcon />}
+                onClick={visitTeachersPage}
+              >
+                Teachers page
+              </Button>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} md={8}>
+            <Box sx={{ my: 5 }}>
+              <Chatbox chat={exampleChat} />
+            </Box>
+          </Grid>
+        </Grid>
+
+        {process.env.NEXT_PUBLIC_NODE_ENV === 'development' && (
+          <>
+            <Typography variant='h5' sx={{ m: 3, mt: 10, color: 'gray' }}>
+              These link shortcuts only appear in the development environment:
+            </Typography>
+            <Typography variant='h5' sx={{ m: 3 }}>
+              <Link href={`/teacher/classroom/${sampleClassroomName}`}>
+                Visit Teachers admin page
+              </Link>
+            </Typography>
+            <Typography variant='h5' sx={{ m: 3 }}>
+              <Link href={`/student/classroom/${sampleClassroomName}`}>
+                Visit Students classroom page
+              </Link>
+            </Typography>
+          </>
+        )}
       </main>
     </Layout>
   );
