255dc22153b7984ba4abe3f0b9837c2c962a0247
Zach Lankton <zachlankton@gmail.com>
2022-03-11 09:01:13 -0500

    Update pages/_app.tsx
    Co-authored-by: Moshe Siegel <siegel.moshes@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,7 @@ export default function MyApp(props: MyAppProps) {
   ];
   const router = useRouter();
   const [userIsAuthorized, setUserIsAuth] = React.useState(false);
+  /* Prevents Unauthorized Page Access to Teacher or Student Pages by checking what page the app starts on in the browser. If the app starts on a page in the denyList then the user is forced back to the homepage. This way all navigation to protected pages must happen from the homepage. */
   React.useEffect(() => {
     if (denyList.includes(router.pathname)) {
       window.location.assign('/');
