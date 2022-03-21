9567cda44d6ca980b1747a2c387ef13c98271d77
Your Name <karlrez123@gmail.com>
2022-03-21 15:55:11 -0700

    updated as per comments

---
+++ src/components/pages/StudentsPage/CopyButton.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+
+import { Fab } from '@mui/material';
+import { ContentCopy as ContentCopyIcon } from '@mui/icons-material';
+
+export default function CopyButton({ elementId }) {
+  const copyToClipboard = () => {
+    const temp = document.createElement('div');
+    temp.setAttribute('contentEditable', 'true');
+    temp.innerHTML = document.getElementById(elementId).innerHTML;
+    temp.setAttribute(
+      'onfocus',
+      "document.execCommand('selectAll',false,null)",
+    );
+    document.body.appendChild(temp);
+    temp.focus();
+    document.execCommand('copy');
+    document.body.removeChild(temp);
+    alert('Conversation copied to clipboard');
+  };
+  return (
+    <>
+      <Fab
+        variant='extended'
+        size='small'
+        color='primary'
+        style={{
+          marginBottom: '10px',
+          background: '#940000',
+          textTransform: 'none',
+        }}
+        onClick={copyToClipboard}
+      >
+        <ContentCopyIcon />
+        &nbsp;Copy chat
+      </Fab>
+    </>
+  );
+}
