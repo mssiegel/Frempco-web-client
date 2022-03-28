802f49516601a1ef017b73b3288516e4b3b8979d
Your Name <karlrez123@gmail.com>
2022-03-27 19:43:26 -0700

    changed background and text color

--- src/components/pages/StudentsPage/WelcomeMessage.css.tsx
+++ src/components/pages/StudentsPage/WelcomeMessage.css.tsx
@@ -1,11 +1,13 @@
 import { css } from '@emotion/react';
 
 const unpairedMessageContainer = css`
+  background: #f8e5e0;
+  color: red;
   width: 98%;
   max-width: 800px;
   border: 1px solid white;
+  border-radius: 10px;
   padding: 70px 0;
-  color: white;
   text-align: center;
 `;
 
