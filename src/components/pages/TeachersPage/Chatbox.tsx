3604fd4d8bfd0acf3c58ccbd4c0cfacd3883ac95
rblcoder <35212670+rblcoder@users.noreply.github.com>
2023-01-05 13:48:35 +0530

    Teacher can copy displayed chat #6

--- src/components/pages/TeachersPage/Chatbox.tsx
+++ src/components/pages/TeachersPage/Chatbox.tsx
@@ -4,10 +4,12 @@ import { Box } from '@mui/material';
 
 import chatboxCSS from './Chatbox.css';
 import Conversation from './Conversation';
+import CopyButton from '@components/shared/CopyButton';
 
 export default function Chatbox({ chat }) {
   return (
     <Box css={chatboxCSS.chatboxContainer}>
+      <CopyButton elementId='conversation' />
       <Box css={chatboxCSS.chatboxTop}>
         <Conversation chat={chat} />
       </Box>
