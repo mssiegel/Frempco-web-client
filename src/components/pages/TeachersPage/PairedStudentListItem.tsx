6ca2001d728f654c79002a2bd324df3843987d72
Moshe Siegel <siegel.moshes@gmail.com>
2025-10-14 21:51:41 +0300

    Refactor: Moved the "End all chats" button to the PairedStudentsList component

--- src/components/pages/TeachersPage/PairedStudentListItem.tsx
+++ src/components/pages/TeachersPage/PairedStudentListItem.tsx
@@ -16,35 +16,6 @@ export default function PairedStudentListItem({
 }) {
   const socket = useContext(SocketContext);
 
-  function endAllChats() {
-    const endAllChatsConfirmed = confirm(
-      'Are you sure you want to end all the chats?',
-    );
-    if (!endAllChatsConfirmed) return;
-
-    const newUnpairedList = [];
-
-    for (const chat of studentChats) {
-      if (chat.mode === SOLO) {
-        socket.emit('solo mode: end chat', { chatId: chat.chatId });
-        newUnpairedList.push(chat.student);
-      } else {
-        const [student1, student2] = chat.studentPair;
-        socket.emit('unpair student chat', {
-          chatId: chat.chatId,
-          student1,
-          student2,
-        });
-        newUnpairedList.push(student1, student2);
-      }
-    }
-    setStudentChats([]);
-
-    // We concat the 'newUnpairedList' to 'unpaired' in case a new student
-    // joined while this function was running.
-    setUnpairedStudents((unpaired) => [...unpaired, ...newUnpairedList]);
-  }
-
   function endChat(chatId, chatMode, student1, student2) {
     const endChatConfirmed = confirm(
       `Are you sure you want to end the ${
@@ -69,17 +40,6 @@ export default function PairedStudentListItem({
 
   return (
     <>
-      {/* TODO refactor: move the 'end all chats' button to the pairedStudentsList */}
-      <Button
-        sx={{ display: 'block', margin: '8px auto 14px auto' }}
-        variant='contained'
-        size='medium'
-        color='warning'
-        onClick={() => endAllChats()}
-      >
-        End all chats
-      </Button>
-
       {studentChats.map((chat) => {
         const student1 =
           chat.mode === SOLO ? chat.student : chat.studentPair[0];
