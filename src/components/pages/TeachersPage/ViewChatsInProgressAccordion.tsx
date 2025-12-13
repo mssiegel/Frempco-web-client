2e94e6d42f28e455d91af78e5f2adbd544733c4f
Moshe Siegel <siegel.moshes@gmail.com>
2025-12-14 00:31:55 +0200

    Adds text of how many chats are currently in progress

--- src/components/pages/TeachersPage/ViewChatsInProgressAccordion.tsx
+++ src/components/pages/TeachersPage/ViewChatsInProgressAccordion.tsx
@@ -6,6 +6,7 @@ import {
 } from '@mui/material';
 import { ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
 import { StudentChat, SoloChat } from './index';
+import { PAIRED, SOLO } from '@utils/classrooms';
 
 interface ViewChatsInProgressAccordionProps {
   studentChats: (StudentChat | SoloChat)[];
@@ -15,6 +16,12 @@ const ViewChatsInProgressAccordion = ({
   studentChats,
 }: ViewChatsInProgressAccordionProps) => {
   const totalStudents = studentChats.length;
+  const pairCount = studentChats.filter((chat) => chat.mode === PAIRED).length;
+  const soloCount = studentChats.filter((chat) => chat.mode === SOLO).length;
+
+  const verb = pairCount === 1 ? 'is' : 'are';
+  const pairText = pairCount === 1 ? 'pair' : 'pairs';
+  const soloText = soloCount === 1 ? 'solo chat' : 'solo chats';
 
   return (
     <Accordion disableGutters sx={{ boxShadow: 'none', mb: 3 }}>
@@ -26,7 +33,19 @@ const ViewChatsInProgressAccordion = ({
           Step 3: View Chats In Progress ({totalStudents})
         </Typography>
       </AccordionSummary>
-      <AccordionDetails>{/* Content will be added here */}</AccordionDetails>
+      <AccordionDetails>
+        <Typography fontFamily='Lora' fontSize='16px'>
+          There {verb}{' '}
+          <strong>
+            {pairCount} {pairText} of student chats
+          </strong>{' '}
+          and{' '}
+          <strong>
+            {soloCount} {soloText}
+          </strong>
+          .
+        </Typography>
+      </AccordionDetails>
     </Accordion>
   );
 };
