2e041f569366f777a41aa00f9fc7fa4a3c97a4af
Moshe Siegel <siegel.moshes@gmail.com>
2026-02-10 14:45:29 +0200

    Makes the hero title mobile-friendly

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, useMediaQuery, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import { useContext } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
@@ -22,6 +23,8 @@ export default function HomePage() {
   const apiUrl = `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1`;
   const socket = useContext(SocketContext);
   const { setUser } = useContext(UserContext);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   async function visitStudentsPage(classroom: string, student: string) {
     const getResponse = await fetch(`${apiUrl}/classrooms/${classroom}`);
@@ -67,8 +70,8 @@ export default function HomePage() {
           display='flex'
           justifyContent='center'
         >
-          <Box width='700px'>
-            <Typography variant='h2' mb={3}>
+          <Box maxWidth='700px'>
+            <Typography variant={isMobile ? 'h3' : 'h2'} mb={3}>
               Bring Learning to Life Through{' '}
               <Box component='span' color='primary.500'>
                 Student Role-Play
