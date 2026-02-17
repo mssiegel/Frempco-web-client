fae323ea52c6a072ac278ced797acb71bd3ffd39
Moshe Siegel <siegel.moshes@gmail.com>
2026-02-17 12:43:24 +0200

    extracts homepage sections into their own components

---
+++ src/components/pages/HomePage/CoreValues.tsx
@@ -0,0 +1,3 @@
+export default function CoreValues(): JSX.Element | null {
+  return null;
+}
---
+++ src/components/pages/HomePage/Hero.tsx
@@ -0,0 +1,68 @@
+/** @jsxImportSource @emotion/react */
+import { Box, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+import StudentsButton from './StudentsButton';
+import TeachersButton from './TeachersButton';
+import StudentBenefitsList from './StudentBenefitsList';
+import RoleplayMasks from '../../../../public/roleplayMasks.png';
+
+interface HeroProps {
+  isMobile: boolean;
+  gameButtonsRef: React.RefObject<HTMLDivElement>;
+  visitStudentsPage: (classroom: string, student: string) => void;
+  visitTeachersPage: (classroom: string) => void;
+}
+
+export default function Hero({
+  isMobile,
+  gameButtonsRef,
+  visitStudentsPage,
+  visitTeachersPage,
+}: HeroProps) {
+  return (
+    <Grid px={1} container color='black'>
+      <Grid
+        item
+        sm={12}
+        md={6}
+        textAlign='center'
+        display='flex'
+        justifyContent='center'
+      >
+        <Box>
+          <Typography variant={isMobile ? 'h3' : 'h2'} mb={3}>
+            Bring Learning to Life Through{' '}
+            <Box component='span' color='primary.500'>
+              Student Role-Play
+            </Box>
+          </Typography>
+          <Image
+            src={RoleplayMasks}
+            alt='Roleplaying masks'
+            priority={true}
+            width={250}
+            style={{
+              maxWidth: '100%',
+              height: 'auto',
+            }}
+          />
+          <Box ref={gameButtonsRef} my={6} display='flex' gap={1}>
+            <StudentsButton visitStudentsPage={visitStudentsPage} />
+            <TeachersButton visitTeachersPage={visitTeachersPage} />
+          </Box>
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        sm={12}
+        md={6}
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <StudentBenefitsList />
+      </Grid>
+    </Grid>
+  );
+}
