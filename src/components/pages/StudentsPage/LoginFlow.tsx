6cd4aca24493e093d50bec10018c6855b354c51e
Moshe Siegel <siegel.moshes@gmail.com>
2026-02-26 21:09:08 +0200

    Added input for PIN

--- src/components/pages/StudentsPage/LoginFlow.tsx
+++ src/components/pages/StudentsPage/LoginFlow.tsx
@@ -1,8 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
+import { Box, TextField } from '@mui/material';
+
+const PIN_LENGTH = 4;
+
 interface LoginFlowProps {
-  pin: number;
-  setPin: React.Dispatch<React.SetStateAction<number>>;
+  pin: number | undefined;
+  setPin: React.Dispatch<React.SetStateAction<number | undefined>>;
   setName: React.Dispatch<React.SetStateAction<string>>;
 }
 
@@ -11,5 +15,45 @@ export default function LoginFlow({
   setPin,
   setName,
 }: LoginFlowProps): JSX.Element {
-  return <div>Login Flow placeholder</div>;
+  return (
+    <Box>
+      <TextField
+        autoFocus
+        placeholder='Game PIN'
+        variant='outlined'
+        inputProps={{
+          inputMode: 'numeric',
+          pattern: '[0-9]*',
+          maxLength: PIN_LENGTH,
+        }}
+        sx={{
+          width: 200,
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '16px',
+            backgroundColor: 'neutrals.white',
+            boxShadow: '3px 3px 0 0 rgba(0, 0, 0, 0.25)',
+            '& input': {
+              p: '16px',
+              color: 'neutrals.500',
+              textAlign: 'center',
+              fontSize: '32px',
+            },
+            '& fieldset': {
+              border: '1px solid silver',
+            },
+            '&.Mui-focused fieldset': {
+              border: '1px solid silver',
+            },
+            '&:hover fieldset': {
+              border: '1px solid silver',
+            },
+          },
+          '& .MuiInputBase-input::placeholder': {
+            color: 'neutrals.500',
+            opacity: 1,
+          },
+        }}
+      />
+    </Box>
+  );
 }
