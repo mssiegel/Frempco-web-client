138b16de0f7a9b986fa2a0b62b9de286a42527fa
Moshe Siegel <siegel.moshes@gmail.com>
2026-02-27 10:22:53 +0200

    Updated header on homepage to include frempco logo

--- src/components/pages/StudentsPage/Header.tsx
+++ src/components/pages/StudentsPage/Header.tsx
@@ -12,12 +12,23 @@ export default function Header({ isMobile }: HeaderProps): JSX.Element {
       sx={{
         backgroundColor: 'primary.500',
         color: 'neutrals.200',
-        py: '24px',
+        py: '10px',
         px: isMobile ? '40px' : '80px',
         borderBottom: '2px solid neutrals.200',
       }}
     >
-      Header placeholder
+      <Box display='flex' gap={2} alignItems='flex-end'>
+        <img
+          src='/StudentsPage/frempco-logo-icon.svg'
+          alt='Frempco logo icon'
+          style={{ height: 28, width: 'auto' }}
+        />
+        <img
+          src='/StudentsPage/frempco-logo-text.svg'
+          alt='Frempco logo text'
+          style={{ height: 24, width: 'auto' }}
+        />
+      </Box>
     </Box>
   );
 }
