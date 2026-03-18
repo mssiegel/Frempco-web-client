fde688045d86d555699e0273a8f4fd01a9146de4
Moshe Siegel <siegel.moshes@gmail.com>
2026-03-18 12:21:07 +0200

    Students name in the header for chatbox

--- src/components/pages/StudentsPage/Header.tsx
+++ src/components/pages/StudentsPage/Header.tsx
@@ -5,11 +5,13 @@ import { Box, Typography } from '@mui/material';
 interface HeaderProps {
   isMobile: boolean;
   statusText: string;
+  studentName?: string;
 }
 
 export default function Header({
   isMobile,
   statusText,
+  studentName,
 }: HeaderProps): JSX.Element {
   return (
     <Box
@@ -25,16 +27,24 @@ export default function Header({
       }}
     >
       <Box display='flex' gap={2} alignItems='flex-end'>
-        <img
-          src='/StudentsPage/frempco-logo-icon.svg'
-          alt='Frempco logo icon'
-          style={{ height: 28, width: 'auto' }}
-        />
-        <img
-          src='/StudentsPage/frempco-logo-text.svg'
-          alt='Frempco logo text'
-          style={{ height: 24, width: 'auto' }}
-        />
+        {studentName ? (
+          <Typography variant='h4' sx={{ color: 'neutrals.white' }}>
+            {studentName}
+          </Typography>
+        ) : (
+          <>
+            <img
+              src='/StudentsPage/frempco-logo-icon.svg'
+              alt='Frempco logo icon'
+              style={{ height: 28, width: 'auto' }}
+            />
+            <img
+              src='/StudentsPage/frempco-logo-text.svg'
+              alt='Frempco logo text'
+              style={{ height: 24, width: 'auto' }}
+            />
+          </>
+        )}
       </Box>
       {!isMobile && statusText && (
         <Typography variant='h4' sx={{ color: 'neutrals.white' }}>
--- src/components/pages/StudentsPage/index.tsx
+++ src/components/pages/StudentsPage/index.tsx
@@ -184,7 +184,11 @@ export default function StudentsPage(): JSX.Element {
       }}
     >
       <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0 }}>
-        <Header isMobile={isMobile} statusText={headerStatusText} />
+        <Header
+          isMobile={isMobile}
+          statusText={headerStatusText}
+          studentName={studentName || undefined}
+        />
       </Box>
       {!studentName ? (
         <LoginFlow
