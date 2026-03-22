73faa5491d2cfe3daab1c70526aed88adfadc1dd
Moshe Siegel <siegel.moshes@gmail.com>
2026-03-22 15:46:54 +0200

    Teachers Chatbox Header now matches the Students chatbox header

--- src/components/pages/TeachersPage/Chatbox/Header.tsx
+++ src/components/pages/TeachersPage/Chatbox/Header.tsx
@@ -1,20 +1,6 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import { ChatParticipants } from '../types';
-
-const headerSx = {
-  root: {
-    fontStyle: 'italic',
-    color: 'gray',
-    mb: '5px',
-  },
-  student1: {
-    color: 'primary.500',
-  },
-  student2: {
-    color: 'secondary.600',
-  },
-};
 
 interface HeaderProps {
   participants: ChatParticipants;
@@ -22,25 +8,47 @@ interface HeaderProps {
 
 export default function Header({ participants }: HeaderProps) {
   const { student1, student2 } = participants;
+  const characterRows = [
+    {
+      label: student1.realName + ':',
+      value: student1.character,
+    },
+    {
+      label: student2?.realName ? student2.realName + ':' : '',
+      value: student2?.character ?? 'chatbot',
+    },
+  ];
 
   return (
-    <Box sx={headerSx.root}>
-      <Box component='span' mr={2}>
-        ({student1.realName})
-      </Box>
-      <Box component='span' sx={headerSx.student1}>
-        {student1.character}
-      </Box>
-      <br />
-      {student2 && (
-        <Box component='span' mr={2}>
-          {student2.realName}
-        </Box>
-      )}
-      <Box component='span' sx={headerSx.student2}>
-        {student2 ? student2.character : 'chatbot'}
-      </Box>
-      <Box>------</Box>
+    <Box
+      sx={{
+        backgroundColor: 'primary.500',
+        borderRadius: '12px 12px 0 0',
+        py: '10px',
+        px: '16px',
+      }}
+    >
+      {characterRows.map(({ label, value }) => (
+        <CharacterRow key={label} label={label} value={value} />
+      ))}
+    </Box>
+  );
+}
+
+interface CharacterRowProps {
+  label: string;
+  value: string;
+}
+
+function CharacterRow({ label, value }: CharacterRowProps) {
+  return (
+    <Box sx={{ display: 'flex', gap: 0.5 }}>
+      <Typography variant='body2' sx={{ color: 'neutrals.200' }}>
+        {label}
+      </Typography>
+      <Typography variant='body2' sx={{ color: 'neutrals.white' }}>
+        {value}
+      </Typography>
     </Box>
   );
 }
