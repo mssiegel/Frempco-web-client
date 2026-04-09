5c317c3fafde6a4de9134233bfba19d2f9c26047
Karl Rezansoff <karlrezansoff@Karls-MacBook-Air.local>
2026-04-09 19:09:28 -0400

    address PR comments for example chats usage

--- src/components/pages/TeachersPage/ActiveGameRoom/CompletedChatsAccordion/index.tsx
+++ src/components/pages/TeachersPage/ActiveGameRoom/CompletedChatsAccordion/index.tsx
@@ -6,7 +6,7 @@ import {
 } from '@mui/material';
 import { ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
 import { StudentChat, SoloChat } from '../../types';
-import DisplayOfChats from '../ChatsInProgressAccordion/DisplayOfChats';
+import DisplayOfChats from '../shared/DisplayOfChats';
 
 interface CompletedChatsAccordionProps {
   studentChats: (StudentChat | SoloChat)[];
@@ -15,6 +15,10 @@ interface CompletedChatsAccordionProps {
 const CompletedChatsAccordion = ({
   studentChats,
 }: CompletedChatsAccordionProps): JSX.Element => {
+  const chatCount = studentChats.length;
+  const verb = chatCount === 1 ? 'was' : 'were';
+  const chatText = chatCount === 1 ? 'chat' : 'chats';
+
   return (
     <Accordion disableGutters sx={{ boxShadow: 'none', mb: 3 }}>
       <AccordionSummary
@@ -22,15 +26,18 @@ const CompletedChatsAccordion = ({
         sx={{ borderRadius: '15px', border: '1px solid black', gap: 2 }}
       >
         <Typography variant='h5' fontWeight={400}>
-          Step 4: View Completed Chats
+          Completed Chats
         </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <DisplayOfChats
-          studentChats={studentChats}
-          setStudentChats={null}
-          setUnpairedStudents={null}
-        />
+        <Typography variant='body2'>
+          There {verb}{' '}
+          <strong>
+            {chatCount} {chatText}
+          </strong>{' '}
+          completed.
+        </Typography>
+        <DisplayOfChats studentChats={studentChats} />
       </AccordionDetails>
     </Accordion>
   );
