7c793365c69aed73b921f6566914a786297bdeae
Moshe Siegel <siegel.moshes@gmail.com>
2026-04-13 15:06:58 +0300

    added unpaired students text to teachers page header

--- src/components/pages/TeachersPage/InProgressActivity/index.tsx
+++ src/components/pages/TeachersPage/InProgressActivity/index.tsx
@@ -13,6 +13,7 @@ import ChatsInProgressAccordion from './ChatsInProgressAccordion';
 import CompletedChatsAccordion from './CompletedChatsAccordion';
 import featureFlags from '@config/featureFlags';
 import { EXAMPLE_CHATS } from '../exampleChats';
+import PageHeader from '@components/shared/PageHeader';
 
 interface InProgressActivityProps {
   activityPin: string;
@@ -159,16 +160,27 @@ export default function InProgressActivity({
     return (
       <Box my={10}>
         <Typography variant='h4' textAlign='center'>
-          You are no longer connected to this activity on Frempco. Return to
-          the <Link href='/'>Frempco homepage</Link> and start another
-          activity.
+          You are no longer connected to this activity on Frempco. Return to the{' '}
+          <Link href='/'>Frempco homepage</Link> and start another activity.
         </Typography>
       </Box>
     );
   }
 
+  const importantStudentTotalsText = (
+    <Typography component='span' variant='h4'>
+      Students waiting to chat: <strong>{unpairedStudents.length}</strong>
+    </Typography>
+  );
+
   return (
     <main>
+      <PageHeader
+        leftElement={importantStudentTotalsText}
+        statusText={'Activity in Progress'}
+        isSticky={true}
+      />
+
       <Box mx={2}>
         <Box mb={3}>
           <Typography variant='h5' mb={1}>
