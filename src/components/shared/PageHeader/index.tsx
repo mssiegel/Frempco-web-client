ded4eb50db02873f9a9f8d39e095125fa5aa1fd7
Moshe Siegel <siegel.moshes@gmail.com>
2026-04-13 14:09:39 +0300

    Added header to create activity

---
+++ src/components/shared/PageHeader/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Typography, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+interface PageHeaderProps {
+  leftElement: JSX.Element;
+  statusText: string;
+}
+
+export default function PageHeader({
+  leftElement,
+  statusText,
+}: PageHeaderProps): JSX.Element {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.500',
+        color: 'neutrals.white',
+        py: '10px',
+        px: isMobile ? '40px' : '80px',
+        borderBottom: '2px solid',
+        borderBottomColor: 'neutrals.200',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      {leftElement}
+      {!isMobile && statusText && (
+        <Typography variant='h4'>{statusText}</Typography>
+      )}
+    </Box>
+  );
+}
