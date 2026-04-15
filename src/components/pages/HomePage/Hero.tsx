7f125d41f13644011642651116e73e8fb2c56d9a
Moshe Siegel <siegel.moshes@gmail.com>
2026-04-15 22:05:58 +0300

    updated homepage text and images

--- src/components/pages/HomePage/Hero.tsx
+++ src/components/pages/HomePage/Hero.tsx
@@ -1,10 +1,9 @@
-/** @jsxImportSource @emotion/react */
-
 import { Box, Grid, Typography } from '@mui/material';
 import Image from 'next/image';
 import StudentsButton from './StudentsButton';
 import TeachersButton from './TeachersButton';
-import exampleRoleplayImage from '../../../../public/HomePage/homepage-example-roleplay.png';
+import studentViewDesktopImage from '../../../../public/HomePage/student-view-of-a-chat-desktop.png';
+import studentViewMobileImage from '../../../../public/HomePage/student-view-of-a-chat-mobile.png';
 
 interface HeroProps {
   isMobile: boolean;
@@ -36,13 +35,13 @@ export default function Hero({
           <Typography variant={isMobile ? 'h3' : 'h2'} mb={3}>
             Bring Learning to Life Through{' '}
             <Box component='span' color='primary.500'>
-              Role-Play
+              Role Play
             </Box>
           </Typography>
           <Typography variant='body1' color='neutrals.400'>
             A <strong>classroom activity</strong> that excites both{' '}
-            <strong>teachers</strong> and <strong>students</strong>. No
-            accounts, fast setup, <strong>100% free</strong>.
+            <strong>teachers</strong> and <strong>students</strong>. No accounts
+            needed. <strong>100% free</strong>.
           </Typography>
 
           <Box
@@ -50,7 +49,7 @@ export default function Hero({
             my={6}
             display='flex'
             flexDirection={isMobile ? 'column' : 'row'}
-            gap={1}
+            gap={2}
           >
             <StudentsButton
               visitStudentsPage={visitStudentsPage}
@@ -70,13 +69,13 @@ export default function Hero({
       {/* Image - 4 columns */}
       <Grid item md={4} sx={{ margin: 'auto' }}>
         <Image
-          src={exampleRoleplayImage}
-          alt='Example roleplay between two students'
+          src={isMobile ? studentViewMobileImage : studentViewDesktopImage}
+          alt='View of a student chatting as the ghost of Julius Caesar'
           priority={true}
           style={{
-            maxWidth: '320px',
+            maxWidth: '450px',
             height: 'auto',
-            border: '2px solid silver',
+            border: '1px solid silver',
             borderRadius: '18px',
           }}
         />
